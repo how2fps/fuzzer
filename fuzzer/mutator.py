4a9fe1bcf8f0d8a465d40ef12ee5f06da13a7829
how2fps <hearthstonesigh@gmail.com>
2026-02-22 16:01:11 +0800

    fix: convert submodules to regular folders

--- fuzzer/mutator.py
+++ fuzzer/mutator.py
@@ -1,6 +1,10 @@
 import random
 
 
+#'["foo", {"bar":["baz", null, 1.0, 2]}]'
+#'{"__complex__": true, "real": 1, "imag": 2}'
+#'{"json":"obj"}'
+#need to add json specific ones, and ipv4/ipv6 ones
 class Mutator:
         def __init__(self):
                 self.interesting_8 = 0
@@ -9,7 +13,7 @@ class Mutator:
                 res = data[:]
                 if not res: return bytearray(b"0")
         
-                for _ in range(random.randint(1, 128)):
+                for _ in range(random.randint(1, 10)):
                         method = random.choice([
                                 self.bit_flip, self.arithmetic, self.interesting_value, 
                                 self.delete_block, self.clone_block
@@ -17,6 +21,7 @@ class Mutator:
                         res = method(res)
                 return res
 
+        #BASIC BIT MUTATES
         def bit_flip(self, data):
                 idx = random.randrange(len(data))
                 data[idx] ^= (1 << random.randrange(8))
@@ -25,27 +30,16 @@ class Mutator:
         def arithmetic(self, data):
                 idx = random.randrange(len(data))
 
-                val = random.randint(1, 35)
-                if random.choice():
-                        data[idx] = (data[idx] + val) % 256
-                else:
-                        data[idx] = (data[idx] - val) % 256
-                return data
-
         def interesting_value(self, data):
                 idx = random.randrange(len(data))
-                val = random.choice(self.interesting_8)
-                data[idx] = val
-                return data
+                
 
         def delete_block(self, data):
                 if len(data) < 2: return data
                 idx = random.randrange(len(data))
-                length = random.randint(1, len(data) - idx)
-                del data[idx:idx+length]
-                return data
+               
 
         def clone_block(self, data):
                 idx = random.randrange(len(data))
-                data.insert(idx, data[idx])
-                return data
+                
+        
